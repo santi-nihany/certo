1fe146efef868c83d545815be5163217cc8b0720
Bautista <bautax1234@gmail.com>
2024-08-24 14:54:55 -0300

    FV

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ import Image from "next/image"
 
 const navItems = [
   { href: "/participant/available", label: "Participate" },
-  { href: "/create", label: "Create Survey" },
+  { href: "/researcher/dashboard", label: "My Surveys" },
 ]
 
 export default function Navbar() {
