d3e86c446b482800e9e1076422f9a9bdccb7b679
Bautista <bautax1234@gmail.com>
2024-08-24 11:17:48 -0300

    Configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = IBM_Plex_Mono({
+  weight: ["100", "200", "300", "400", "700", ], 
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
