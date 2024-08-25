812e712e215f7c49f4b868edb188e840b6968f87
santi-nihany <santi.nihany@gmail.com>
2024-08-25 01:02:28 -0300

    wallet login + interaction script

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Header from "@/app/components/session/header";
 
 export default function Home() {
   return (
-    <div className="bg-dark text-primary mx-auto flex flex-col items-center justify-center">
-      <Image
-        className="mx-auto mt-20"
-        src="/certo-logo.svg"
-        alt="CERTO"
-        width={1000}
-        height={1000}
-      />
+    <div>
+      <Header />
+      <div className="bg-dark text-primary mx-auto flex flex-col items-center justify-center">
+        <Image
+          className="mx-auto mt-20"
+          src="/certo-logo.svg"
+          alt="CERTO"
+          width={1000}
+          height={1000}
+        />
+      </div>
     </div>
   );
 }
